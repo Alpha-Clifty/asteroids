bd47acef4f12188aaf098372260ddffcf5419665
Alpha-Clifty <kc_majic@hotmail.com>
2025-07-01 18:25:56 +0100

    adding player

--- main.py
+++ main.py
@@ -3,16 +3,24 @@ from constants import *
 
 def main():
 	pygame.init()
+	clock = pygame.time.Clock()
+	dt = 0
 	screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+	print("Starting Asteroids!")
+	print("Screen width:",SCREEN_WIDTH)
+	print("Screen height:",SCREEN_HEIGHT)
+
 	while True:
 		for event in pygame.event.get():
 			if event.type == pygame.QUIT:
 				return
 		screen.fill((0,0,0))
+		Player
 		pygame.display.flip()
-	print("Starting Asteroids!")
-	print("Screen width:",SCREEN_WIDTH)
-	print("Screen height:",SCREEN_HEIGHT)
+		dt = clock.tick(60)
+		dt /= 1000
+
+
 
 if __name__ == "__main__":
 	main()
